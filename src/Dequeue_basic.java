bcad3e314065533ef561520b76b513db0887a319
Sreeramya <sreeramya1205@gmail.com>
2026-03-19 00:40:54 +0530

    add array deque basic file

---
+++ src/Dequeue_basic.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Dequeue_basic {
+    public static void main(String[] args) {
+        Deque<Integer> d1=new ArrayDeque<>();
+        d1.add(34);
+        d1.add(78);
+        d1.add(89);
+    }
+}
