645e299537b0d935c55c16f62c278297298058e5
Sreeramya <sreeramya1205@gmail.com>
2026-03-19 00:38:47 +0530

    added queue_basic file

---
+++ src/Queue_basic.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_basic {
+    public static void main(String[] args) {
+        Queue<Integer> q1=new LinkedList<>();
+        q1.add(34);
+        q1.add(56);
+        q1.add(67);
+    }
+}
