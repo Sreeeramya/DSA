855d40757a0915d20566772d4dd7da34f79b66b0
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-03-19 00:46:33 +0530

    Update Dequeue_basic.java

--- src/Dequeue_basic.java
+++ src/Dequeue_basic.java
@@ -7,5 +7,17 @@ public class Dequeue_basic {
         d1.add(34);
         d1.add(78);
         d1.add(89);
+        d1.addFirst(67);
+        d1.addLast(77);
+        for(int a:d1){
+            System.out.print(a+" ");
+        }
+        System.out.println();
+        System.out.println(d1.remove());
+        System.out.println(d1.removeFirst());
+        System.out.println(d1.removeLast());
+        for(int a:d1){
+            System.out.print(a+" ");
+        }
     }
 }
