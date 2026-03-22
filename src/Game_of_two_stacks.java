aba4288bd307575c4938e583b6546024dfb4d6a4
Sreeramya <sreeramya1205@gmail.com>
2026-03-22 21:36:19 +0530

    added game of stacks

---
+++ src/Game_of_two_stacks.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game_of_two_stacks {
+    public static void main(String[] args) {
+
+
+    }
+}
